baf68f4b93d673241bc8ec22a78ccb1aef831255
Vedant <vedantsarnobat@gmail.com>
2025-02-23 12:02:59 +0530

    GetMoviesFromID API Created

---
+++ BL/MoviesBL.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Nodes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace RedisCaching.BL
+{
+    public class MoviesBL
+    {
+        private readonly IConfiguration _config;
+        public MoviesBL(IConfiguration config) 
+        { 
+            _config = config;
+        }
+
+        public async Task<JObject> GetMoviesFromIDAsync(int id)
+        {
+            JObject obj = new();
+            string url = $"{_config.GetSection("MoviesURL").Value}{id}/details/";
+            try
+            {
+                var client = new RestClient();
+                var request = new RestRequest(url, Method.Get)
+                    .AddQueryParameter("apiKey", _config.GetSection("APIKey").Value)
+                    .AddQueryParameter("language", "en-US") 
+                    .AddQueryParameter("region", "US");
+
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+
+                var response = await client.ExecuteAsync(request);
+                if (response.IsSuccessful && !string.IsNullOrWhiteSpace(response.Content))
+                {
+                    obj = JObject.Parse(response.Content);
+                    return obj;
+                }
+                else
+                {
+                    obj.Add("status", "failed");
+                    obj.Add("msg", $"API call failed. Status: {response.StatusCode}, Error: {response.ErrorMessage}");
+                    return obj;
+                }
+            }
+            catch (Exception ex) 
+            {
+                obj.Add("status", "failed");
+                obj.Add("msg", $"Exception while fetching response: {ex}");
+                return obj;
+            }
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RedisCaching.BL;
+
+namespace RedisCaching.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IConfiguration _config;
+        public MoviesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Route("/api/Movies/GetMoviesFromID")]
+        [HttpPost]
+        public async Task<IActionResult> GetMoviesFromID(int id)
+        {
+            MoviesBL moviesBL = new(_config);
+            JObject res = await moviesBL.GetMoviesFromIDAsync(id);
+            return Ok(res);
+        }
+    }
+}
